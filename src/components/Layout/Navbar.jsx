4b40219d8e9c2ddcb38e4c3225be7887bfae3544
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2026-03-14 09:44:34 +0300

    updated navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full top-0 z-50 bg-blue-900 shadow-lg py-2.5">
+    <nav className="fixed w-full top-0 z-50 bg-transparent py-2.5">
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
 
         {/* Logo + Company Name */}
@@ -13,50 +13,51 @@ export default function Navbar() {
             className="h-12 w-auto object-contain"
           />
 
-          <span className="text-white font-bold text-lg md:text-xl">
+          <span className="text-yellow-400 font-bold text-lg md:text-xl">
             Civieways Construction Ltd
           </span>
         </Link>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex space-x-8 text-white font-medium">
+        <div className="hidden md:flex space-x-8 text-yellow-400 font-medium">
 
           <Link
             to="/"
-            className="hover:text-yellow-400 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Home
           </Link>
 
           <Link
             to="/about"
-            className="hover:text-yellow-400 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             About
           </Link>
 
           <Link
             to="/services"
-            className="hover:text-yellow-400 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Services
           </Link>
 
           <Link
             to="/projects"
-            className="hover:text-yellow-400 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Projects
           </Link>
 
           <Link
             to="/contact"
-            className="hover:text-yellow-400 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Contact
           </Link>
 
         </div>
+
       </div>
     </nav>
   );
